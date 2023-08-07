51f4414b1d03d7eaca5f956ef3643d251f77ef44
cseiberling <89859574+cseiberling@users.noreply.github.com>
2023-08-07 12:29:37 -0500

    Create install-local-packages.sh

---
+++ install-local-packages.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# Globals
+PACKAGE_DIR='/opt/packages'
+CL_VERSION='23.12.7'
+
+# Install generic debian packages.
+apt install -y ${PACKAGE_DIR}/*.deb
+
+# Install docker.
+dpkg -i ${PACKAGE_DIR}/containerd.io_*.deb \
+  ${PACKAGE_DIR}/docker-ce_*.deb \
+  ${PACKAGE_DIR}/docker-ce-cli_*.deb \
+  ${PACKAGE_DIR}/docker-buildx-plugin_*.deb \
+  ${PACKAGE_DIR}/docker-compose-plugin_*.deb
+
+# Install docker-compose.
+mv ${PACKAGE_DIR}/docker-compose-Linux-x86_64 ${PACKAGE_DIR}/docker-compose
+mv ${PACKAGE_DIR}/docker-compose /usr/local/bin/
+chmod +x /usr/local/bin/docker-compose
+
+# Install open-jdk 11.
+mkdir /usr/lib/jvm
+tar -zxf ${PACKAGE_DIR}/openjdk-11+28_linux-x64_bin.tar.gz -C /usr/lib/jvm
+update-alternatives --install /usr/bin/java java /usr/lib/jvm/java-11-openjdk-amd64/bin/java 100
+update-alternatives --install /usr/bin/javac javac /usr/lib/jvm/java-11-openjdk-amd64/bin/javac 100
+update-alternatives --display java
+update-alternatives --display javac
+
+# Install CodeLogic.
+apt install -y ${PACKAGE_DIR}/codelogic_${CL_VERSION}_all.deb
