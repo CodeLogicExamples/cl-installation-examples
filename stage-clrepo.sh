e3d5f11f64edfc20b2fffa8c9a6f6a4333e9b6e2
cseiberling <89859574+cseiberling@users.noreply.github.com>
2023-08-08 17:05:40 -0500

    Delete stage-clrepo.sh

--- stage-clrepo.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-PACKAGE_DIR='/opt/packages'
-
-echo "deb [trusted=yes] file:///${PACKAGE_DIR}/cl-dependencies clpackagedir/ >> /etc/apt/sources.list.d/cl-packagedir.list"
-dpkg-scanpackages ${PACKAGE_DIR}/cl-dependencies | gzip > ${PACKAGE_DIR}/cl-dependencies/cl-packages.gz
-apt-get update
