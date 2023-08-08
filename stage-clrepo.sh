4f17fc023b4b44e77d1940c892e13eacc361a112
cseiberling <89859574+cseiberling@users.noreply.github.com>
2023-08-08 09:10:41 -0500

    Update stage-clrepo.sh

--- stage-clrepo.sh
+++ stage-clrepo.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 
-PACKAGE_DIR='/opt/packages'
-mkdir ${PACKAGE_DIR}
-chmod 766 ${PACKAGE_DIR}
-
 echo "deb [trusted=yes] file:///${PACKAGE_DIR}/cl-dependencies clpackagedir/ >> /etc/apt/sources.list.d/clpackagedir.list"
-dpkg-scanpackages
+dpkg-scanpackages ${PACKAGE_DIR}/cl-dependencies
